6c30bce9daa19a73dc97cd70853283c729c65f19
Remco Tolsma <info@remcotolsma.nl>
2015-11-19 13:08:10 +0100

    Extend from the iDEAL Basic integration.

--- src/Integration.php
+++ src/Integration.php
@@ -1,22 +1,10 @@
 <?php
 
-class Pronamic_WP_Pay_Gateways_FrieslandBank_IDealZakelijk_Integration {
+class Pronamic_WP_Pay_Gateways_FrieslandBank_IDealZakelijk_Integration extends Pronamic_WP_Pay_Gateways_IDealBasic_AbstractIntegration {
 	public function __construct() {
 		$this->id         = 'frieslandbank-ideal-zakelijk';
 		$this->name       = 'Friesland Bank - iDEAL Zakelijk';
 		$this->provider   = 'frieslandbank';
 		$this->deprecated = true;
-	}
-
-	public function get_config_factory_class() {
-		return 'Pronamic_WP_Pay_Gateways_IDealBasic_ConfigFactory';
-	}
-
-	public function get_config_class() {
-		return 'Pronamic_WP_Pay_Gateways_IDealBasic_Config';
-	}
-
-	public function get_gateway_class() {
-		return 'Pronamic_WP_Pay_Gateways_IDealBasic_Gateway';
 	}
 }
